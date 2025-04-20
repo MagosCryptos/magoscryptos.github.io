165cc6be6a2f4d53ccd6e63ea13e18de5809cf46
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2025-04-20 19:06:29 +0300

    google pages adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,21 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
 
 export default config;
